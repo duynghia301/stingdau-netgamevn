9698aed773e16a85bc258dbecbc0133ac395cd7d
Nghia <nghiabd21@uef.edu.vn>
2023-10-10 18:49:19 +0700

    bang nap tien test

---
+++ PROJECT_NetGameVN_STINGDAU/PROJECT_NetGameVN_STINGDAU/NapTien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJECT_NetGameVN_STINGDAU
+{
+    public partial class NapTien : Form
+    {
+        public NapTien()
+        {
+            InitializeComponent();
+        }
+    }
+}
