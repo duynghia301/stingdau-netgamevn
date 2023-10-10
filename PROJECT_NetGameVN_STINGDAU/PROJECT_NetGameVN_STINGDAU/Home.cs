e585ded733fe9926d12549e723a0c27d84fcf29b
Nghia <nghiabd21@uef.edu.vn>
2023-10-10 16:53:34 +0700

    add new

---
+++ PROJECT_NetGameVN_STINGDAU/PROJECT_NetGameVN_STINGDAU/Home.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJECT_NetGameVN_STINGDAU
+{
+    public partial class frmMayChu : Form
+    {
+        public frmMayChu()
+        {
+            InitializeComponent();
+        }
+        void ResizeTabs()
+        {
+            int numTabs = tabControl1.TabCount;
+
+            float totLen = 0;
+            using (Graphics g = CreateGraphics())
+            {
+                // Get total length of the text of each Tab name
+                for (int i = 0; i < numTabs; i++)
+                    totLen += g.MeasureString(tabControl1.TabPages[i].Text, tabControl1.Font).Width;
+            }
+
+            int newX = (int)((tabControl1.Width - totLen) / numTabs) / 2;
+            tabControl1.Padding = new Point(newX, tabControl1.Padding.Y);
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void frmMayChu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
